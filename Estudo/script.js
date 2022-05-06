6bca623fdc7264802b50cd2d4f23053024f0c076
Will <willam.castro@outlok.com>
2022-05-06 20:45:10 -0300

    Comparações

---
+++ Estudo/script.js
@@ -0,0 +1,17 @@
+// == igual a 
+// ==== valor igual e igual
+// != não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior ou igual a
+// <= menor ou igual a 
+
+console.log(3 == "3");
+console.log(3 === "3");
+console.log(2 != 2);
+console.log(2 !== "2");
+console.log(2 > 3);
+console.log(2 < 3);
+console.log(2 >= 2);
+console.log(2 <= 2);
